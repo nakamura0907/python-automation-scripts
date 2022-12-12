358b50002ffd264ba48c4381c72646560768c18f
nakamura0907 <nakamura990907@gmail.com>
2022-12-12 20:38:42 +0900

    コマンドライン引数を受け取る

--- image-to-pdf/project/main.py
+++ image-to-pdf/project/main.py
@@ -1,1 +1,15 @@
-print('Image To PDF')
+import argparse
+
+if __name__ == '__main__':
+    # コマンドライン引数
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", help="画像の入力先ディレクトリ", default="./../assets")
+    parser.add_argument("--output", help="PDFの出力先ディレクトリ", default="./../dist")
+
+    args = parser.parse_args()
+
+    # PDFの出力
+
+    print(args)
+    print(args.output)
